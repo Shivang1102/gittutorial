4b232e2a23f3054b50043e5df8745c3315ceb58c
Shivang1102 <67555617+Shivang1102@users.noreply.github.com>
2022-12-22 19:31:44 +0530

    dom updated

---
+++ dom.js
@@ -0,0 +1,30 @@
+// // console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title=123;
+// console.log(document.doctype);
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent="Hello";
+// console.log(document.forms[0]);
+// console.log(document.links);
+
+// console.log(document.getElementById("header-title"));
+// var headertitle = document.getElementById("header-title");
+// var header=document.getElementById("main-header");
+// console.log(headertitle);
+// headertitle.textContent="Hello";
+// headertitle.innerText="Goodbye";
+// headertitle.innerHTML='<h3>Hello</h3>';
+// header.style.borderBottom="solid 3px #000";
+
+var additem= document.getElementById("Add");
+console.log(additem.textContent);
+additem.style.color="green";
+additem.style.fontWeight="bold";
+
+
